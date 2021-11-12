be837373f89a9205ccf97ac3a1d09024dcec1be1
Lawrence Splaver <lsplaver@yahoo.com>
2021-11-11 23:57:44 -0500

    Updated to correctly create new employee records.

--- QuarterlySales/Controllers/EmployeeController.cs
+++ QuarterlySales/Controllers/EmployeeController.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using QuarterlySales.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace QuarterlySales.Controllers
 {
     public class EmployeeController : Controller
     {
+        private QuarterlySalesContext context;
 
+        public EmployeeController(QuarterlySalesContext ctx) => context = ctx;
         public IActionResult Index()
         {
             return View("Index", "Home");
@@ -18,7 +21,19 @@ namespace QuarterlySales.Controllers
         [HttpGet]
         public ViewResult Add()
         {
-            return View(new Employee());
+            QuarterlySalesViewModel vm = new QuarterlySalesViewModel();
+            //int temp = context.Employees.Count();
+            //int[] employeeArray;
+            //employeeArray = new int[temp];
+            //for (int x = 0, y = 1; x < temp; x++, y++)
+            //{
+            //    vm.CurrentEmployee.EmployeeId = context.Employees.Find(y).EmployeeId;
+            //    employeeArray[x] = vm.CurrentEmployee.EmployeeId;
+            //}
+            //ViewBag.EmployeeId = employeeArray;
+            vm.Employees = context.Employees.ToList();
+            vm.Sales = context.Sales.ToList();
+            return View(vm);//new QuarterlySalesViewModel()); //Employee());
         }
 
         public IActionResult Cancel()
@@ -26,10 +41,23 @@ namespace QuarterlySales.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        //[HttpPost]
-        //public IActionResult Add(Employee employee)
-        //{
-
-        //}
+        [HttpPost]
+        public IActionResult Add(QuarterlySalesViewModel employee)
+        {
+            QuarterlySalesViewModel vm = new QuarterlySalesViewModel();
+            if (ModelState.IsValid)
+            {
+                context.Employees.Add(employee.CurrentEmployee);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                vm.Employees = context.Employees.ToList();
+                vm.Sales = context.Sales.ToList();
+                ModelState.AddModelError("", "Please correct all errors.");
+                return View(vm);
+            }
+        }
     }
 }
--- QuarterlySales/Models/QuarterlySalesViewModel.cs
+++ QuarterlySales/Models/QuarterlySalesViewModel.cs
@@ -11,6 +11,33 @@ namespace QuarterlySales.Models
         public List<Employee> Employees { get; set; }
         public string Name { get; set; }
         public double TotalSales { get; set; }
+        //public QuarterlySalesViewModel()
+        //{
+        //    CurrentEmployee = new Employee();
+        //    CurrentSale = new Sale();
+        //}
+        //public QuarterlySalesViewModel(int id)
+        //{
+        //    CurrentEmployee = new Employee();
+        //    CurrentSale = new Sale();
+        //    Name = Employees.Find(id).FirstName + " " + Employees.Find(id).LastName;
+        //}
+        public Employee CurrentEmployee { get; set; }
+        public Sale CurrentSale { get; set; }
+
+        //public QuarterlySalesViewModel()
+        //{
+        //    if (CurrentEmployee.EmployeeId.ToString() != null)
+        //    {
+        //        EmployeeStringId = CurrentEmployee.EmployeeId.ToString();
+        //    }
+        //    else
+        //    {
+        //        EmployeeStringId = "0";
+        //    }
+        //}
+
+        public string EmployeeStringId { get; set; }
 
         //public int id { get; }
 
