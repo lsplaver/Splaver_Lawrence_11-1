6c8f776e992790add45efe78928f71a893f42470
Lawrence Splaver <lsplaver@yahoo.com>
2021-11-14 15:03:42 -0500

    Migrated SaleController and Sales/Add from EmployeeName-column-added branch to master branch.  Updated code to comment out unneeded code and fixed custom validation.

--- QuarterlySales/Controllers/SalesController.cs
+++ QuarterlySales/Controllers/SalesController.cs
@@ -3,14 +3,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using QuarterlySales.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace QuarterlySales.Controllers
 {
     public class SalesController : Controller
     {
+        private QuarterlySalesContext context;
+
+        public SalesController(QuarterlySalesContext ctx) => context = ctx;
+
         public IActionResult Index()
         {
-            return View();
+            return View("Index", "Home");
+        }
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            QuarterlySalesViewModel vm = new QuarterlySalesViewModel();
+            vm.Sales = context.Sales.ToList();
+            vm.Employees = context.Employees.ToList();
+            return View(vm);
+        }
+
+        public IActionResult Cancel()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Add(QuarterlySalesViewModel sale)
+        {
+            QuarterlySalesViewModel vm = new QuarterlySalesViewModel();
+            vm.Sales = context.Sales.ToList();
+            vm.Employees = context.Employees.ToList();
+            Sale checkQuarter = context.Sales.FirstOrDefault(s => s.Quarter == sale.CurrentSale.Quarter);
+            Sale checkYear = context.Sales.FirstOrDefault(s => s.Year == sale.CurrentSale.Year);
+            Sale checkEmployee = context.Sales.FirstOrDefault(s => s.EmployeeId == sale.CurrentSale.EmployeeId);
+            //string firstName = vm.Employees.Find(e => e.EmployeeId == checkEmployee.EmployeeId).FirstName;
+            //string lastName = vm.Employees.Find(e => e.EmployeeId == checkEmployee.EmployeeId).LastName;
+            string fullName = context.Employees.Find(sale.CurrentSale.EmployeeId).FullName;
+
+            if (checkQuarter != null && checkYear != null && checkEmployee != null)
+            {
+                ModelState.AddModelError("CurrentSale.EmployeeId", $"Sales for {/*firstName} {lastName*//*sale.CurrentSale.Employee.FullName*/fullName} for {sale.CurrentSale.Year} Q{sale.CurrentSale.Quarter} are already in the database");
+            }
+
+            if (ModelState.IsValid)
+            {
+                context.Sales.Add(sale.CurrentSale);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                vm.Sales = context.Sales.ToList();
+                vm.Employees = context.Employees.ToList();
+                ModelState.AddModelError("", "Please correct all errors");
+                return View(vm);
+            }
         }
     }
 }
